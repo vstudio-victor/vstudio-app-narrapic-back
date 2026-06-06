1e916c5e0df3a82986772ddb86becdae7156eb17
vvictorvoicu <victor_voicu61@yahoo.com>
2026-06-06 15:38:41 +0200

    cors 2

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   });
 
   // Serve contracts folder as static
